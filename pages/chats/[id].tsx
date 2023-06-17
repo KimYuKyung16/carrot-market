9fcf07ae63c0bc08f68c4f496ef5c607140c7ce4
KimYuKyung <kyg0954@naver.com>
2023-06-18 00:50:54 +0900

    test

--- pages/chats/[id].tsx
+++ pages/chats/[id].tsx
@@ -47,7 +47,7 @@ interface ReviewForm {
   review: string;
 }
 
-const socket = io("kcarrotmarket.store:5000");
+const socket = io("kcarrotmarket.store:5000", { transports: ['websocket'] });
 const ChatDetail: NextPage = () => {
   const router = useRouter();
   const { register, handleSubmit } = useForm<ReviewForm>();
