cb7ac25cdd3f7f456cbc726b2c8a3bce1ede135a
KimYuKyung <kyg0954@naver.com>
2023-06-17 19:03:55 +0900

    change address

--- pages/chats/[id].tsx
+++ pages/chats/[id].tsx
@@ -47,7 +47,7 @@ interface ReviewForm {
   review: string;
 }
 
-const socket = io("ec2-13-125-237-101.ap-northeast-2.compute.amazonaws.com:443", { transports: ['websocket'] });
+const socket = io("ec2-13-125-237-101.ap-northeast-2.compute.amazonaws.com:443");
 const ChatDetail: NextPage = () => {
   const router = useRouter();
   const { register, handleSubmit } = useForm<ReviewForm>();
