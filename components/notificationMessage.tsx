d0a65f61b17475a3b4cdfd6f0868884eaccc0989
KimYuKyung <kyg0954@naver.com>
2023-06-17 18:39:00 +0900

    change address

--- components/notificationMessage.tsx
+++ components/notificationMessage.tsx
@@ -38,7 +38,7 @@ interface ProductResponse {
   ok: boolean;
 }
 
-const socket = io("ec2-13-125-237-101.ap-northeast-2.compute.amazonaws.com:5000", {transports: ['websocket']});
+const socket = io("ec2-13-125-237-101.ap-northeast-2.compute.amazonaws.com:5000");
 export default function NotificationMessage({
   message,
   avatarUrl,
